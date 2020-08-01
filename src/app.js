c218f31e5ce0fd149c80f9c7316ab9228d01c929
Nguyen Chi Dung <dungnc@gmail.com>
2020-08-01 22:02:11 +0700

    - Add config file

--- src/app.js
+++ src/app.js
@@ -55,6 +55,17 @@ app.get("/refresh", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Listening on https://apple.ananay.dev");
-});
+app.set("port", process.env.PORT || 5000);
+
+//For avoidong Heroku $PORT error
+app
+  .get("/", function (request, response) {
+    var result = "App is running";
+    response.send(result);
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });
